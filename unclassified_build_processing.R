b6a41253e1d0f0c3c7271095b6f02215e4c3364b
scar2.0 <martin.baierl@jacobs.com>
2020-03-10 13:29:28 +0000

    Added trip rate calculations Fixed wrong coding of cars Removed typos

--- unclassified_build_processing.R
+++ unclassified_build_processing.R
@@ -1,6 +1,7 @@
-# Organising NTS data and trip rates for regression analysis and selection
+### Organising NTS data and trip rates for regression analysis and selection ###
 
 
+### Load libraries and import data ##############################################
 library("tidyverse")
 library("survey")
 library("sjstats")
@@ -9,6 +10,7 @@ library("compare")
 
 select <- dplyr::select
 
+
 # Import weighted weekly trip rates
 # (!) 518 parsing failures
 week_weighted_trips <- read_csv("Y:/NTS/weekly_trip_rates.csv")
@@ -16,6 +18,9 @@ week_weighted_trips <- read_csv("Y:/NTS/weekly_trip_rates.csv")
 
 # Import unclassified build
 unclassified_build <- read_csv("Y:/NTS/tfn_unclassified_build.csv")
+
+
+### Prepare dataset #############################################################
 
 
 # Recode trip purposes as NTEM classification hb_purpose
@@ -63,7 +68,7 @@ unclassified_build <- unclassified_build %>%
 
 # Combine Age and work status to age_workstatus
 unclassified_build <- unclassified_build %>%
-  mutate(age_Work_status = case_when(
+  mutate(age_work_status = case_when(
     Age_B01ID %in% c(1,2,3,4,5) ~ '0-16_child',
     Age_B01ID %in% c(6,7,8,9,10,11,12,13,14,15,16,17,18) & EcoStat_B01ID %in% c(1,2) ~ '16-74_fte',
     Age_B01ID %in% c(6,7,8,9,10,11,12,13,14,15,16,17,18) & EcoStat_B01ID %in% c(3,4) ~ '16-74_pte',
@@ -79,9 +84,9 @@ unclassified_build <- unclassified_build %>%
 # Recode number of cars(NumCarVan_B02ID) as cars
 unclassified_build <- unclassified_build %>%
   mutate(cars = case_when(
-  NumCarVan_B02ID == 0 ~ '0',
-  NumCarVan_B02ID == 1 ~ '1',
-  NumCarVan_B02ID %in% c(2,3) ~ '2+',
+  NumCarVan_B02ID == 1 ~ '0',
+  NumCarVan_B02ID == 2 ~ '1',
+  NumCarVan_B02ID == 3 ~ '2+',
     TRUE ~ as.character(NumCarVan_B02ID)
   ))
 
@@ -130,6 +135,7 @@ unclassified_build <- unclassified_build %>%
   TRUE ~ as.character(HHoldAreaType1_B01ID)
 ))
 
+
 # Drop area_type = -8
 unclassified_build <- subset(unclassified_build, area_type != -8)
 
@@ -174,6 +180,7 @@ unclassified_build <- unclassified_build %>%
     TRUE ~ as.character(MainMode_B04ID)
   ))
 
+
 # Create time_period from day(TravDay) and hour(TripTravTime)
 # Confirm peak hours
 unclassified_build <- unclassified_build %>%
@@ -187,37 +194,143 @@ unclassified_build <- unclassified_build %>%
     TRUE ~ as.character('unclassified')
   ))
 
+
 # Drop unclassified time_period
 unclassified_build <- subset(unclassified_build, time_period != 'unclassified')
 
 
 # Transform catgeorical variables to factors
-facts <- c("hb_purpose", "age_Work_status", "gender", "hh_adults", "cars", "area_type", "soc_cat", "ns_sec", "main_mode", "time_period")
+facts <- c("hb_purpose", "age_work_status", "gender", "hh_adults", "cars", "area_type", "soc_cat", "ns_sec", "main_mode", "time_period")
 unclassified_build <- unclassified_build %>% mutate_at(facts, funs(factor))
 
 
+### Analysis to remove incomplete surveys for prior years.
+# Obtain unique households
+unq_household <- unclassified_build %>%
+  select(SurveyYear, HouseholdID, OutCom_B02ID) %>%
+  distinct()
 
-## to do:
+
+# Count individuals per household, remove missing IndividualID 
+household_individual_count <- unclassified_build %>%
+  filter(!is.na(IndividualID)) %>%
+  select(SurveyYear, HouseholdID, IndividualID) %>%
+  distinct() %>%
+  group_by(SurveyYear, HouseholdID) %>%
+  count() %>%
+  rename(individuals = n)
+
+
+# Count trips per household, remove missing TripID
+household_trip_count <- unclassified_build %>%
+  filter(!is.na(TripID)) %>%
+  select(SurveyYear, HouseholdID, TripID) %>%
+  distinct() %>%
+  group_by(SurveyYear, HouseholdID) %>%
+  count() %>%
+  rename(trips = n)
+
+
+# Count days per household, remove missing TripID
+household_days_count <- unclassified_build %>%
+  filter(!is.na(TripID)) %>%
+  select(SurveyYear, HouseholdID, TravDay) %>%
+  distinct() %>%
+  group_by(SurveyYear, HouseholdID) %>%
+  count() %>%
+  rename(days = n)
+
+
+# Count trip distance by household, remove missing TripDisIncSW
+household_trip_distance <- unclassified_build %>%
+  filter(!is.na(TripDisIncSW)) %>%
+  select(SurveyYear, HouseholdID, TripDisIncSW) %>%
+  distinct() %>%
+  group_by(SurveyYear, HouseholdID) %>%
+  count() %>%
+  rename(logged_distance = n)
+
+
+# Count travel time by household, remove missing TripTravTime
+household_trip_time <- unclassified_build %>%
+  filter(!is.na(TripTravTime)) %>%
+  select(SurveyYear, HouseholdID, TripTravTime) %>%
+  distinct() %>%
+  group_by(SurveyYear, HouseholdID) %>%
+  count() %>%
+  rename(logged_time = n)
+
+
+# Obtain unique households with individuals, days, trips, distance and time, replace NAs
+unq_household <- unq_household %>%
+  left_join(household_individual_count) %>%
+  left_join(household_trip_count) %>%
+  left_join(household_days_count) %>%
+  left_join(household_trip_distance) %>%
+  left_join(household_trip_time) %>%
+  mutate(individuals = replace_na(individuals, 0)) %>%
+  mutate(trips = replace_na(trips, 0)) %>%
+  mutate(days = replace_na(days, 0)) %>%
+  mutate(logged_distance = replace_na(logged_distance, 0)) %>%
+  mutate(logged_time = replace_na(logged_time, 0))
+
+
+# Remove households with 0 trips (no trips = 0 left, but kept in script)
+usable_households <- unq_household %>%
+  filter(trips > 0) %>%
+  select(SurveyYear, HouseholdID)
+
+
+# Join usable households to unclassified_build
+unclassified_build <- usable_households %>% left_join(unclassified_build)
+
+
+
+### Weekly trip rate calculations (Ian Williams NTS Report Method) #####################
+
+# Step 1 - Weighted trips and trip sample by segment
+week_total_trips <- unclassified_build %>%
+  mutate(weighted_trip = W1 * W5xHh * W2) %>%
+  group_by(hb_purpose, age_work_status, gender, hh_adults, cars, soc_cat, ns_sec, area_type) %>%
+  summarise(trip_sample = n(), weekly_trips = sum(weighted_trip, na.rm=TRUE)) %>%
+  ungroup()
+
+
+# Step 2 - Get sum of weighted individuals
+### Necessary to include hb_purpose? ###???### Yes because person_sample calculation?
+week_total_individuals <- unclassified_build %>%
+  select(IndividualID, hb_purpose, age_work_status, gender, hh_adults, cars, soc_cat, ns_sec, area_type, W1, W2) %>%
+  unique() %>%
+  mutate(person_weight = W1 * W2) %>%
+  group_by(hb_purpose, age_work_status, gender, hh_adults, cars, soc_cat, ns_sec, area_type, area_type) %>%
+  summarise(person_sample = n(), person_weight = sum(W2)) %>%
+  ungroup()
+
+
+# Step 3 - Divide total trips by weighted sum of individuals
+week_weighted_trips <- week_total_trips %>%
+  left_join(week_total_individuals,
+            by = c('hb_purpose', 'age_work_status', 'gender', 'hh_adults', 'cars', 'soc_cat', 'ns_sec', 'area_type')) %>%
+  mutate(tfn_trip_rate = weekly_trips/person_weight)
+
+week_weighted_trips %>% write_csv('weekly_trip_rates.csv')
+
+
+
+
+## to do/check:
 
 
 # 1) check if soc classification still correct
-# 2) apply soc classification only apply for trip purposes 1 & 2? - necessary?
+# 2) apply soc classification only apply for trip purposes 1 & 2?
 # 3) missing: job accessibility by rail, job accessibility by highway  - CS to include?
 # 4) not included household_comp as not clear where from, also likely highly correlated with household_adults
 # 5) cars recoding depending on adults in household?
-# 6) trip rate calculations?
+# 6) need to segment by main_mode and time_period for mode/time regression?
 
 
 
 
-### required for trip rate calculations:
-#PSUID
-#HouseholdID
-#IndividualID 
-##OutCom_B02ID
-#SurveyYear
-#TripID
-#TravDay 
 
 
 
