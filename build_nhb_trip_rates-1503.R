0d41603446a90c8f9028e2f8c5ecd104fd204b52
chrisbstorey <58773847+chrisbstorey@users.noreply.github.com>
2021-03-17 17:52:45 +0000

    new extraction

--- build_nhb_trip_rates-1503.R
+++ build_nhb_trip_rates-1503.R
@@ -13,19 +13,32 @@ classified_build <- classified_build %>%
   mutate(trip_weights = W1 * W5 * W2) %>%
   lu_ca()
 
+# Set up hb and nhb mode
+classified_build <- classified_build %>%
+  mutate(hb_mode = main_mode,
+         nhb_mode = main_mode,
+         )
+
+
+# What's going on with proportions
+classified_build %>%
+  group_by(trip_origin) %>%
+  count()
+
+# NEED home based mode
 # SOC CATS
 hb_trips <- classified_build %>%
   filter(trip_origin == 'hb') %>%
   filter(TravDay %in% c(1,2,3,4,5)) %>%
   select(trip_origin,
          hb_purpose,
-         main_mode,
+         hb_mode,
          soc_cat,
          ns_sec,
          trip_weights) %>%
   group_by(trip_origin,
            hb_purpose,
-           main_mode,
+           hb_mode,
            soc_cat,
            ns_sec) %>%
   summarise(hb_trips = sum(trip_weights)) %>%
@@ -34,95 +47,87 @@ hb_trips <- classified_build %>%
 
 nhb_trips <- classified_build %>%
   filter(trip_origin == 'nhb') %>%
+  filter(nhb_purpose != 99) %>%
   filter(TravDay %in% c(1,2,3,4,5)) %>%
   select(trip_origin,
+         nhb_mode,
          nhb_purpose_hb_leg,
          nhb_purpose,
-         main_mode,
-         
-         tfn_area_type,
-         ca,
          soc_cat,
-         trip_origin,
-         ,
+         ns_sec,
          trip_weights) %>%
-  group_by(tfn_area_type,
-           ca,
+  group_by(trip_origin,
+           nhb_mode,
+           nhb_purpose_hb_leg,
+           nhb_purpose,
            soc_cat,
-           trip_origin,
-           nhb_purpose_hb_leg,
-           nhb_purpose) %>%
+           ns_sec) %>%
   summarise(nhb_trips = sum(trip_weights)) %>%
   ungroup() %>%
   rename(hb_purpose = nhb_purpose_hb_leg) %>%
   select(-trip_origin)
 
-soc_trip_rates <- hb_trips %>%
-  left_join(nhb_trips, by=c('tfn_area_type','ca', 'soc_cat', 'hb_purpose')) %>%
+
+# Separate out and retain soc somehow 
+
+soc_hb <- hb_trips %>%
+  select(-ns_sec) %>%
+  filter(hb_purpose %in% c(1,2)) %>%
+  group_by(hb_purpose, hb_mode, soc_cat) %>%
+  summarise(hb_trips = sum(hb_trips, na.rm=TRUE)) %>%
+  ungroup()
+
+ns_sec_hb <- hb_trips %>%
+  select(-soc_cat) %>%
+  filter(hb_purpose %in% c(3,4,5,6,7,8)) %>%
+  group_by(hb_purpose, hb_mode, ns_sec) %>%
+  summarise(hb_trips=sum(hb_trips, na.rm = TRUE)) %>%
+  ungroup()
+
+soc_nhb <- nhb_trips %>%
+  select(-ns_sec) %>%
+  filter(nhb_purpose == 12) %>%
+  group_by(nhb_mode, hb_purpose, nhb_purpose, soc_cat) %>%
+  summarise(nhb_trips=sum(nhb_trips, na.rm = TRUE)) %>%
+  ungroup()
+
+ns_sec_nhb <- nhb_trips %>%
+  select(-soc_cat) %>%
+  filter(nhb_purpose != 12) %>%
+  group_by(nhb_mode, hb_purpose, nhb_purpose, ns_sec) %>%
+  summarise(nhb_trips = sum(nhb_trips, na.rm = TRUE)) %>%
+  ungroup()
+
+# Bind drop null segs, infill NA
+# 99 == SOC 0, need join value for NA so using 999
+hb_trips <- bind_rows(soc_hb, ns_sec_hb) %>%
+  mutate(soc_cat = case_when(soc_cat == 99 ~ 0,
+                             is.na(soc_cat) ~ 999,
+                             TRUE ~ soc_cat)) %>%
+  mutate(ns_sec = case_when(is.na(ns_sec) ~ 999,
+                            TRUE ~ ns_sec))
+
+nhb_trips <- bind_rows(soc_nhb, ns_sec_nhb) %>%
+  mutate(soc_cat = case_when(soc_cat == 99 ~ 0,
+                             is.na(soc_cat) ~ 999,
+                             TRUE ~ soc_cat)) %>%
+  mutate(ns_sec = case_when(is.na(ns_sec) ~ 999,
+                            TRUE ~ ns_sec))
+
+# Get trip rate
+trip_rates <- hb_trips %>%
+  left_join(nhb_trips, by=c('hb_purpose','soc_cat','ns_sec')) %>%
   mutate(trip_rate = (nhb_trips/hb_trips)) %>%
-  filter(nhb_purpose %in% c(12)) %>%
-  filter(hb_purpose != 99) %>%
-  filter(nhb_purpose != 99) %>%
-  filter(soc_cat != 99) %>%
-  rename(p = hb_purpose) %>%
-  rename(nhb_p = nhb_purpose) %>%
-  rename(area_type = tfn_area_type) %>%
-  select(-hb_trips, -nhb_trips) %>%
-  mutate(ns_sec = 99)
+  filter(ns_sec != -9,
+         hb_purpose != 99,
+         nhb_purpose != 99) %>%
+  rename(p = hb_purpose,
+         nhb_p = nhb_purpose)
 
-# NS Sec
-hb_trips <- classified_build %>%
-  filter(trip_origin == 'hb') %>%
-  filter(TravDay %in% c(1,2,3,4,5)) %>%
-  select(tfn_area_type,
-         ca,
-         ns_sec,
-         trip_origin,
-         hb_purpose,
-         trip_weights) %>%
-  group_by(tfn_area_type,
-           ca,
-           ns_sec,
-           trip_origin,
-           hb_purpose) %>%
-  summarise(hb_trips = sum(trip_weights)) %>%
-  ungroup() %>%
-  select(-trip_origin)
+trip_rates %>% write_csv(paste0(nts_dir, 'outputs/nhb_ave_wday_enh_trip_rates_TEST.csv'))
 
-nhb_trips <- classified_build %>%
-  filter(trip_origin == 'nhb') %>%
-  filter(TravDay %in% c(1,2,3,4,5)) %>%
-  select(tfn_area_type,
-         ca,
-         ns_sec,
-         trip_origin,
-         nhb_purpose_hb_leg,
-         nhb_purpose,
-         trip_weights) %>%
-  group_by(tfn_area_type,
-           ca,
-           ns_sec,
-           trip_origin,
-           nhb_purpose_hb_leg,
-           nhb_purpose) %>%
-  summarise(nhb_trips = sum(trip_weights)) %>%
-  ungroup() %>%
-  rename(hb_purpose = nhb_purpose_hb_leg) %>%
-  select(-trip_origin)
 
-ns_trip_rates <- hb_trips %>%
-  left_join(nhb_trips, by=c('tfn_area_type','ca', 'ns_sec', 'hb_purpose')) %>%
-  mutate(trip_rate = (nhb_trips/hb_trips)) %>%
-  filter(nhb_purpose %in% c(13,14,15,16,18)) %>%
-  filter(hb_purpose != 99) %>%
-  filter(nhb_purpose != 99) %>%
-  filter(ns_sec != -9) %>%
-  rename(p = hb_purpose) %>%
-  rename(nhb_p = nhb_purpose) %>%
-  rename(area_type = tfn_area_type) %>%
-  select(-hb_trips, -nhb_trips) %>%
-  mutate(soc_cat = 99)
-
+####
 ## SOC 0 - default
 
 hb_trips <- classified_build %>%
@@ -171,7 +176,7 @@ soc_zero_trip_rates <- hb_trips %>%
   rename(area_type = tfn_area_type) %>%
   select(-hb_trips, -nhb_trips) %>%
   mutate(ns_sec = 99, soc_cat = 0)
-s
+
 trip_rates <- bind_rows(soc_zero_trip_rates, soc_trip_rates, ns_trip_rates)
 
 # Benchmarking
