73069f4331089653f59722f63a4624e6c2c5174f
Humza-TFN <61422104+Humza-TFN@users.noreply.github.com>
2021-03-24 13:37:57 +0000

    Update classified_build_processing.R
    Updated with 19 build
    - 05-08, 11 survey years are missing

--- classified_build_processing.R
+++ classified_build_processing.R
@@ -1,42 +1,50 @@
-library("tidyverse")
+library(readr)
+library(stringr)
+library(tidyr)
+library(dplyr)
+library(vroom)
+library(purrr)
 
-# Initialise Directories --------------------------------------------------
+save_processed = TRUE
+version = "hb_trip_rates"
+
+# Initialise Directories/Read CSVS/Source R scripts ----------------------------
+
+# Change this to your username and redirect to github folder if not in documents
+username <- Sys.info()[[6]]
 
 # NTS folder
 nts_dir <- "Y:/NTS/"
 
-# Outputs folder
-output_dir <- str_c(nts_dir, "outputs/trip_rates/")
-
 # Import folder
 import_dir <- str_c(nts_dir, "import/")
 
-# Unclassified build
-unclassified_build_dir <- str_c(import_dir, "tfn_unclassified_build.csv")
+# Export folder
+export_dir <- str_c(import_dir, "classified builds")
 
-# Lookups
+# Column names
+column_names_dir <- str_c(import_dir, "variables_hb_trip_rates.csv")
+column_names <- read_csv(column_names_dir)
+column_names <- pull(column_names)
+
+# Read in unclassified build
+unclassified_build_dir <- str_c(import_dir, "tfn_unclassified_build_no_stage19.csv")
+unclassified_build <- vroom(unclassified_build_dir, col_select = column_names)
+
 lookup_dir <- str_c(nts_dir, "lookups/")
+lookups_dir <- str_c("C:/Users/Pluto/Documents/GitHub/NTS-Processing/lookups.R")
 
-# Lookup functions
-source(str_c(lookup_dir,"lookups.r"))
+if(file.exists(lookups_dir)){
+  
+  source(lookups_dir)
+  
+} else {
+  
+  stop("Github Folder not in documents - add custom path 'lookups_dir' to lookups.R in NTS Processing")
+  
+}
 
-# Read in unclassified_build
-unclassified_build <- read_csv(unclassified_build_dir)
-
-# Recode purposes ---------------------------------------------------------
-
-# Adjust classifications to exclude walking trips in day trips
-unclassified_build <- unclassified_build %>%
-  filter(TripPurpTo_B01ID %in% 1:14 |
-           (TripPurpTo_B01ID == 15 & MainMode_B04ID != 1) |
-           TripPurpTo_B01ID %in% 16:23)
-
-unclassified_build <- unclassified_build %>%
-  filter(TripPurpFrom_B01ID %in% 1:14 |
-           (TripPurpFrom_B01ID == 15 & MainMode_B04ID != 1) |
-           TripPurpFrom_B01ID %in% 16:23)
-
-# Define trip origin, hb_purpose, nhb_purpose, nhb_purpose_hb_leg and trip_purpose
+# Classify Purposes -------------------------------------------------------
 unclassified_build <- unclassified_build %>%
   lu_trip_origin() %>% 
   lu_hb_purpose() %>%
@@ -44,43 +52,56 @@ unclassified_build <- unclassified_build %>%
   lu_nhb_purpose_hb_leg() %>% 
   mutate(trip_purpose = ifelse(trip_origin == "hb", hb_purpose, nhb_purpose))
 
-# Recode exploratory variables --------------------------------------------
+# Classify Other variables ------------------------------------------------
 unclassified_build <- unclassified_build %>%
   lu_gender() %>% 
   lu_age_work_status() %>% 
   lu_cars() %>% 
   lu_hh_adults() %>% 
-  lu_area_type() %>%
   lu_soc_cat() %>%
   lu_main_mode() %>%
   lu_start_time() %>% 
   lu_end_time() %>%
   lu_tfn_area_type() %>%
-  lu_ca() %>%
-  rename(ns_sec = NSSec_B03ID)
+  mutate(ns_sec = ifelse(NSSec_B03ID == -9, 99, NSSec_B03ID))
 
-# Reweight short walk trips by a factor of 7 ------------------------------
-classified_build_pre_weight <- unclassified_build %>%
-  mutate(W5xHh = ifelse(TripDisIncSW < 1 & main_mode == 1, 7, W5xHh)) %>%
-  filter(!is.na(W5xHh))
+# Short Walk Weighting
+unclassified_build <- unclassified_build %>%
+  mutate(sw_weight = ifelse(TripDisIncSW < 1 & main_mode == 1, 7, 1))
 
-classified_build_pre_weight %>% write_csv(str_c(import_dir,"classified_nts_pre-weighting.csv"))
+# Save classified build
+if(save_processed) write_csv(unclassified_build, "C:/Users/Pluto/Documents/NTS/classified_build.csv")
 
-# Weighting Methodology -------------------------------------
-weighted_trip_rates <- classified_build_pre_weight %>%
-  filter(trip_purpose %in% 1:8) %>%
-  mutate(trip_weights = W1 * W5xHh * W2) %>%
-  group_by(IndividualID, trip_purpose, SurveyYear, age_work_status, gender, hh_adults, cars, soc_cat, ns_sec, tfn_area_type, W2) %>%
-  summarise(trip_weights = sum(trip_weights),
-            weekly_trips = n())%>%
-  ungroup() %>%
-  mutate(trip_rate = trip_weights/W2,
-         trip_purpose = as.integer(trip_purpose)) %>%
-  select(-trip_weights, -W2)
-
-classified_build_weight <- weighted_trip_rates %>%
-  complete(nesting(IndividualID, SurveyYear, age_work_status, gender, hh_adults, cars, soc_cat, ns_sec, tfn_area_type),
-           trip_purpose = 1:8,
-           fill = list(weekly_trips = 0, trip_rate = 0))
-
-classified_build_weight %>% write_csv(str_c(output_dir,"classified_nts_trip_rates.csv"))
+if(version == "hb_trip_rates"){
+  
+  weighted_trip_rates <- classified_build_pre_weight %>% 
+    filter(trip_purpose %in% 1:8) %>%
+    mutate(trip_weight = W1 * sw_weight * W5 * W2) %>% 
+    group_by(IndividualID, trip_purpose, SurveyYear, age_work_status, gender, hh_adults, cars, soc_cat, ns_sec, tfn_area_type, W2) %>% 
+    summarise(trip_weights = sum(trip_weight),
+              weekly_trips = sum(W1 * sw_weight)) %>% 
+    ungroup() %>% 
+    mutate(trip_rate = trip_weights)
+  
+  classified_build_weight <- weighted_trip_rates %>%
+    mutate(trip_purpose = as.integer(trip_purpose)) %>% 
+    complete(nesting(IndividualID, SurveyYear, age_work_status, gender, hh_adults, cars, soc_cat, ns_sec, tfn_area_type),
+             trip_purpose = 1:8,
+             fill = list(weekly_trips = 0, trip_weights = 0, trip_rate = 0, W2 = 0))
+  
+  write_csv(classified_build_weight, "C:/Users/Pluto/Documents/NTS/hb_trip_rates_build.csv")
+  
+} else if(version == "car_availability"){
+  
+  car_availability <- classified_build_pre_weight %>% 
+    select(EcoStat_B01ID, NumCarVan_B02ID, HHoldNumAdults, HHoldOSWard_B01ID, tfn_area_type, W1, W3) %>% 
+    group_by(EcoStat_B01ID, HHoldOSWard_B01ID, HHoldNumAdults, NumCarVan_B02ID, tfn_area_type) %>% 
+    summarise(weighted_count = sum(W1 * W3),
+              count = n()) %>% 
+    ungroup() %>% 
+    arrange(EcoStat_B01ID, HHoldOSWard_B01ID, HHoldNumAdults, tfn_area_type)
+  
+  write_csv(car_availability, "C:/Users/Pluto/Documents/NTS/car_availability_build.csv")
+  
+}
+  
